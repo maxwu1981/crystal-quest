f41ab9e3c5ccf068fc3b448210014fb79302c687
barry <finalaaaa@gmail.com>
2026-09-08 00:21:32 +0700

    上线前的快照

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // 每次都先问网络只会让弱网下开局卡住。资源变了靠版本号换缓存名来更新。
 //
 // 共 230 个文件，约 3.3 MB。
-const VERSION = '5ffc35bb212e';
+const VERSION = '661712922c91';
 const CACHE = 'crystal-quest-' + VERSION;
 
 const PRECACHE = [
